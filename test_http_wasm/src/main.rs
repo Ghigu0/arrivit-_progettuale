4eea3aa18603c251ae92d98dc48851c0585cf4b7
Francessco <f.binni03@gmail.com>
2026-05-06 11:35:23 +0200

    fix

--- test_http_wasm/src/main.rs
+++ test_http_wasm/src/main.rs
@@ -1,8 +1,28 @@
+use tokio::time::{timeout, Duration};
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     println!("test network");
-    match reqwest::get("http://10.96.0.1:443").await {
-        Ok(r) => println!("OK {}", r.status()),
-        Err(e) => eprintln!("ERR {}", e),
+
+    let url = "http://10.43.217.22:8080";
+
+    println!("calling {}", url);
+
+    let result = timeout(
+        Duration::from_secs(5),
+        reqwest::get(url),
+    )
+    .await;
+
+    match result {
+        Ok(Ok(r)) => {
+            println!("OK {}", r.status());
+        }
+        Ok(Err(e)) => {
+            eprintln!("ERR {}", e);
+        }
+        Err(_) => {
+            eprintln!("TIMEOUT: richiesta bloccata oltre 5 secondi");
+        }
     }
 }
